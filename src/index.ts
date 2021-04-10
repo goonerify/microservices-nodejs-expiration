d4799287ff193fb0c6d1bd939c49b0822677d53d
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-10 17:39:18 -0400

    404. Initial Setup

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID, // Client ID needs to be unique for every instance of the ticketing service
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit(); // exit process entirely when we lose the connection to nats
+    });
+
+    // exit process entirely when the connection is interrupted
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
