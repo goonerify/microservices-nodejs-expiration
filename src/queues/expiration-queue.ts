13118b6b4ab8ad7026fd9ec238e4b96d70038c21
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-10 18:57:21 -0400

    409. Creating a Queue

---
+++ src/queues/expiration-queue.ts
@@ -0,0 +1,23 @@
+import Queue from "bull";
+
+// An interface describing the data that will be stored in this job
+interface Payload {
+  orderId: string;
+}
+
+// order:expiration is the channel in redis where we want to save this job
+const expirationQueue = new Queue<Payload>("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+// Process the job when redis server returns it after elapsed time (15 minutes in this case)
+expirationQueue.process(async (job) => {
+  console.log(
+    "I want to publish an expiration:complete event for orderId",
+    job.data.orderId
+  );
+});
+
+export { expirationQueue };
