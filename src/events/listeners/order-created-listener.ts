b392acf9d0a356845676557a2d9befdc6fcc738a
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-10 20:07:10 -0400

    412. Delaying Job Processing

--- src/events/listeners/order-created-listener.ts
+++ src/events/listeners/order-created-listener.ts
@@ -8,10 +8,18 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    console.log("Waiting this many milliseconds to process the job:", delay);
     // Create a new job to expire this order after the elapsed period of time
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
 
     msg.ack();
   }
